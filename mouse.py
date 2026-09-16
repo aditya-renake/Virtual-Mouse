9cd76d0a4cb3d3afe57c72d52eb5f0b320976db6
aditya-renake <aditya.renake@outlook.com>
2026-09-16 17:09:24 +0530

    in progress

--- mouse.py
+++ mouse.py
@@ -1,7 +1,41 @@
+#import the required libraries here
 import cv2
+import mediapipe as mp
+import util
+
+
+#Set up the mediapipe model which will track the hands
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(
+    static_image_mode = False,
+    model_complexity=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7,
+    max_num_hands=2
+)
+
+
+#Set up the main methods which will be called 
+
+def find_finger_tip(processed):
+    if processed.multi_hand_landmarks:
+        hand_landmarks = processed.multi_hand_landmarks[0]
+        return hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP]
+
+    return None
+
+
+def detect_gestures(frame, landmarks_list, processed):
+    if len(landmarks_list)>=21:
+
+        index_finger_tip = find_finger_tip(processed)
+        print(index_finger_tip)
+
 
 def main():
     cap = cv2.VideoCapture(0)
+    draw = mp.solutions.drawing_utils
     try:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -9,14 +43,40 @@ def main():
             if not ret:
                 break
             frame = cv2.flip(frame, 1)
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+            processed = hands.process(frameRGB)
+            landmarks_list = []
+
+
+            if processed.multi_hand_landmarks:
+                hand_landmarks=processed.multi_hand_landmarks[0]
+                draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+
+                for lm in hand_landmarks.landmark:
+                    landmarks_list.append((lm.x, lm.y))
+
+            detect_gestures(frame, landmarks_list, processed)
+
+            
+
+                
+
 
             cv2.imshow('Frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    finally;
+    finally:
+
         cap.release()
         cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
